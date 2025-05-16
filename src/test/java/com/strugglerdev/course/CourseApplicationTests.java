2a5495af1a0b12ef5a354d1c5bc43bcbd1ccfa5c
Ricardo Jorge <ricardolemos18@hotmail.com>
2025-05-16 16:42:39 -0300

    Project created

---
+++ src/test/java/com/strugglerdev/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strugglerdev.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
